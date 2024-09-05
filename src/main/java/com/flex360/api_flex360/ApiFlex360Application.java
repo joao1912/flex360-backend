8081f9182f221b7d73d7ef2d09bf8acd08476e22
joao <joaopedrot1912@gmail.com>
2024-09-05 13:39:13 -0300

    feat: base do projeto

---
+++ src/main/java/com/flex360/api_flex360/ApiFlex360Application.java
@@ -0,0 +1,13 @@
+package com.flex360.api_flex360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFlex360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFlex360Application.class, args);
+	}
+
+}
