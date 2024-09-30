74d25a707217b6413a2eab8377789afc9eb7d480
Zuma Elma Pereira <zumapereira@hotmail.com>
2024-09-30 16:30:06 -0300

    paguei userDetails

--- src/main/java/com/flex360/api_flex360/repository/AcessorioRepository.java
+++ src/main/java/com/flex360/api_flex360/repository/AcessorioRepository.java
@@ -2,11 +2,10 @@ package com.flex360.api_flex360.repository;
 
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import com.flex360.api_flex360.models.Acessorio;
 
 @Repository
 public interface AcessorioRepository extends JpaRepository<Acessorio, UUID> {
-    UserDetails findByEmDetails(String email);
+    
 }
