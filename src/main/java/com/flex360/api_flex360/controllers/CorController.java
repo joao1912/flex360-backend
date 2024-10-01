36d6530ec1d6f62d81a98b8bba29040ca1bd3a30
Danieli <danielibordulis@gmail.com>
2024-10-01 16:41:03 -0300

    fiz a rota de deletar e de editar

--- src/main/java/com/flex360/api_flex360/controllers/CorController.java
+++ src/main/java/com/flex360/api_flex360/controllers/CorController.java
@@ -12,10 +12,16 @@ import jakarta.el.ELException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/cor")
@@ -46,7 +52,51 @@ public class CorController {
         );
 
     }
+
+    @PostMapping("/criar")
+    public ResponseEntity<CorDTO> criarCor(@RequestBody CorDTO corDTO) {
+        // Converter o CorDTO para a entidade Cor
+        Cor novaCor = new Cor();
+        novaCor.setName(corDTO.nome());
+        novaCor.setCodigo(corDTO.codigo());
+
+        // Chamar o método do serviço para criar a cor
+        Cor corCriada = corService.criarCor(novaCor);
+
+        // Converter a entidade Cor criada para CorDTO para retornar na resposta
+        CorDTO novaCorDTO = new CorDTO(corCriada.getId(), corCriada.getName(), corCriada.getCodigo());
+
+        // Retornar o DTO da nova cor criada com status 201 (Created)
+        return ResponseEntity.status(201).body(novaCorDTO);
+    }
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<CorDTO> editarCor(@PathVariable UUID id, @RequestBody CorDTO corDTO) {
+        // Converter o CorDTO para a entidade Cor
+        Cor corAtualizada = new Cor();
+        corAtualizada.setName(corDTO.nome());
+        corAtualizada.setCodigo(corDTO.codigo());
+
+        // Chamar o serviço para editar a cor existente
+        Cor corEditada = corService.editarCor(id, corAtualizada);
+
+        // Converter a entidade Cor editada para CorDTO para retornar na resposta
+        CorDTO corEditadaDTO = new CorDTO(corEditada.getId(), corEditada.getName(), corEditada.getCodigo());
+
+        // Retornar o DTO da cor editada
+        return ResponseEntity.ok(corEditadaDTO);
+    }
+   
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarCor(@PathVariable UUID id) {
+            // Chamar o serviço para deletar a cor
+            corService.deletarCor(id);
+
+
+
+             return ResponseEntity.ok("Cor deletada com sucesso.");
+        }
     
-    
+
     
 }
