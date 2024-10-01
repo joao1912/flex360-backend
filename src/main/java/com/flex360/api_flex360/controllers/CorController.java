1072bdaf6fd85187cf7f4139eec4af7df39f7ac8
Danieli <danielibordulis@gmail.com>
2024-10-01 16:17:16 -0300

    fiz uma das rotas

--- src/main/java/com/flex360/api_flex360/controllers/CorController.java
+++ src/main/java/com/flex360/api_flex360/controllers/CorController.java
@@ -3,8 +3,50 @@ package com.flex360.api_flex360.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.flex360.api_flex360.dto.auth.LoginResponseDTO;
+import com.flex360.api_flex360.dto.cor.CorDTO;
+import com.flex360.api_flex360.models.Cor;
+import com.flex360.api_flex360.services.CorService;
+
+import jakarta.el.ELException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/cor")
 public class CorController {
+
+    @Autowired
+    private CorService corService;
+    
+    @GetMapping("/buscarTodas")
+    public ResponseEntity<List<CorDTO>> buscarTodasCores() {
+
+        List<Cor> cores=corService.buscarTodasCores();
+
+        throw new ELException();
+
+        
+    }
+
+    @GetMapping("/buscarPorId/{id}")
+    public ResponseEntity<CorDTO> buscarPorId(@RequestParam String id) {
+
+        UUID uuid = UUID.fromString(id);
+       
+        Cor cor= corService.buscarCorPorId(uuid);
+
+        return ResponseEntity.ok(
+            new CorDTO(cor.getId(),cor.getName(), cor.getCodigo())
+        );
+
+    }
+    
+    
     
 }
