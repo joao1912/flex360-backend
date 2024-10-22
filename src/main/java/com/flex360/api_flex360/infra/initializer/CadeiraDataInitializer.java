6a991d68934e1a073cb1bf183f81b19049459819
Joao Pedro <joaopedrot1912@gmail.com>
2024-10-22 20:39:28 -0300

    feat: base do json para cadeiras pronto

---
+++ src/main/java/com/flex360/api_flex360/infra/initializer/CadeiraDataInitializer.java
@@ -0,0 +1,19 @@
+package com.flex360.api_flex360.infra.initializer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CadeiraDataInitializer implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+
+        
+    }
+    
+}
+
