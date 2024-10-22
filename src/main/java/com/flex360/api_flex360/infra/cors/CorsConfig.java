7fe3a8c8353c41dce4fc9f29c3b413493655f330
Joao Pedro <joaopedrot1912@gmail.com>
2024-10-22 12:08:28 -0300

    refactor

--- src/main/java/com/flex360/api_flex360/infra/cors/CorsConfig.java
+++ src/main/java/com/flex360/api_flex360/infra/cors/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.flex360.api_flex360.infra.cors;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +11,9 @@ import org.springframework.lang.NonNull;
 @Configuration
 public class CorsConfig {
 
-    @Value("${cors.allowed-origins:http:localhost:5173}")
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(CorsConfig.class);
+
+    @Value("${cors.allowed-origins}")
     private String allowedOrigins;
 
     @Value("${cors.allowed-methods}")
@@ -27,6 +30,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
+                logger.info("Configuring CORS with allowed origins: {}", allowedOrigins);
                 registry.addMapping("/**")
                         .allowedOrigins(allowedOrigins.split(","))
                         .allowedMethods(allowedMethods.split(","))
