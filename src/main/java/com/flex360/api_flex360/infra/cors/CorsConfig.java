317065f64a191aeef2f28928c73ab6af87d6102b
Joao Pedro <joaopedrot1912@gmail.com>
2024-10-26 15:52:19 -0300

    refactor: cors

--- src/main/java/com/flex360/api_flex360/infra/cors/CorsConfig.java
+++ src/main/java/com/flex360/api_flex360/infra/cors/CorsConfig.java
@@ -1,11 +1,15 @@
 package com.flex360.api_flex360.infra.cors;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.flex360.api_flex360.infra.dotenv.DotenvConfig;
+
 import org.springframework.lang.NonNull;
 import io.github.cdimascio.dotenv.Dotenv;
 
@@ -15,10 +19,10 @@ public class CorsConfig {
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(CorsConfig.class);
     private final Dotenv dotenv;
 
-    public CorsConfig(Dotenv dotenv) {
-        this.dotenv = dotenv;
+    public CorsConfig(DotenvConfig dotenvConfig) {
+        this.dotenv = dotenvConfig.dotenv();
     }
-
+    
     @Value("${cors.allowed-methods}")
     private String allowedMethods;
 
