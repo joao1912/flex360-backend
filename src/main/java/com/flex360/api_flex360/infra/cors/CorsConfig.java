079c5e4ae719a3393033651012c7b004be76b07b
Joao Pedro <joaopedrot1912@gmail.com>
2024-10-26 15:54:11 -0300

    import removido

--- src/main/java/com/flex360/api_flex360/infra/cors/CorsConfig.java
+++ src/main/java/com/flex360/api_flex360/infra/cors/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.flex360.api_flex360.infra.cors;
 
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
