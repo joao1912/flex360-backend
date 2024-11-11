6fbc488cc1d31bef820223176516ec94a6808b25
Dalison <dalisonmg@gmail.com>
2024-11-11 16:22:42 -0300

    refactor: Feito EntityNotFoundException retornar status 404

--- src/main/java/com/flex360/api_flex360/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/flex360/api_flex360/exceptions/GlobalExceptionHandler.java
@@ -3,11 +3,20 @@ package com.flex360.api_flex360.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
         @ExceptionHandler(ErroAoSalvarException.class)
     public ResponseEntity<String> handleErroAoSalvarException(ErroAoSalvarException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
