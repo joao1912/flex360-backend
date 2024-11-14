e912aa86e3a9c9e70c7ba58627a3bbf1f22fa08a
joao <joaopedrot1912@gmail.com>
2024-11-14 14:56:06 -0300

    fix: agora a aplicação aceita token invalidos para rotas publicas

--- src/main/java/com/flex360/api_flex360/infra/security/SecurityFilter.java
+++ src/main/java/com/flex360/api_flex360/infra/security/SecurityFilter.java
@@ -3,6 +3,7 @@ package com.flex360.api_flex360.infra.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -26,36 +27,43 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    @SuppressWarnings("null")
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-        
+
         var token = this.recoverToken(request);
 
-        if (token != null) {
+        if (token != null && requiresAuthentication(request)) {
 
-            var subject = tokenService.validateToken(token);
-            UserDetails user = usuarioRepository.findByEmail(subject);
+            try {
+                var subject = tokenService.validateToken(token);
+                UserDetails user = usuarioRepository.findByEmail(subject);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                if (user != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } catch (Exception e) {
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                System.err.println("Token inválido: " + e.getMessage());
 
+            }
         }
 
         filterChain.doFilter(request, response);
-
     }
 
     private String recoverToken(HttpServletRequest request) {
-        
         var authHeader = request.getHeader("Authorization");
-
-        if (authHeader == null) return null;
-
+        if (authHeader == null) {
+            return null;
+        }
         return authHeader.replace("Bearer ", "");
-
     }
-    
+
+    private boolean requiresAuthentication(HttpServletRequest request) {
+        return SecurityContextHolder.getContext().getAuthentication() == null;
+    }
+
 }
