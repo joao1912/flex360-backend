19277245be38f707b6f106338b266effe2aff9e7
joao <joaopedrot1912@gmail.com>
2024-11-14 15:06:52 -0300

    refactor: mensagem de erro 403 personalizado implementada

---
+++ src/main/java/com/flex360/api_flex360/infra/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package com.flex360.api_flex360.infra.security;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
+            throws IOException, ServletException {
+
+        // Defina o status de erro e a mensagem personalizada
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("{\"message\": \"Você não está autorizado a acessar este recurso.\"}");
+    }
+}
