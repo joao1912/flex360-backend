93e64b39b58a265cda6166aaab3c5c59a4438f1a
Dalison <dalisonmg@gmail.com>
2024-12-02 17:05:37 -0300

    feat: Tentando adicionar verificações de erro

--- src/main/java/com/flex360/api_flex360/infra/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/flex360/api_flex360/infra/security/CustomAuthenticationEntryPoint.java
@@ -1,13 +1,18 @@
 package com.flex360.api_flex360.infra.security;
 
+import java.io.IOException;
+
+import javax.security.auth.login.AccountExpiredException;
+
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -26,7 +31,20 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-        response.getWriter().write(
-                "{\"message\": \"Você não está autenticado ou não possui autorização para acessar este recurso.\"}");
+        String errorMessage = "Você não está autenticado ou não possui autorização para acessar este recurso.";
+
+        if (authException instanceof BadCredentialsException) {
+            errorMessage = "Senha inválida.";
+        } else if (authException instanceof UsernameNotFoundException) {
+            errorMessage = "Usuário não encontrado.";
+        } else {
+            Throwable cause = authException.getCause();
+            if (cause instanceof AccountExpiredException) {
+                errorMessage = "Conta expirada.";
+            }
+
+        }
+
+        response.getWriter().write("{\"message\": \"" + errorMessage + "\"}");
     }
 }
